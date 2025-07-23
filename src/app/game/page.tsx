18bbc67d17c53b7f3e055a13e34b3f651a90604a
Basile Vanhaverbeke <basile.vanha@outlook.fr>
2025-07-23 20:16:08 +0200

    nextjs / ts / prettier / tailwind setup

---
+++ src/app/game/page.tsx
@@ -0,0 +1,10 @@
+import Title from '@/components/atoms/Title';
+
+export default function GamePage() {
+  return (
+    <div className="p-8">
+      <Title>Page du jeu</Title>
+      <p>Bienvenue sur la page de test pour ton futur jeu Unity.</p>
+    </div>
+  );
+}
