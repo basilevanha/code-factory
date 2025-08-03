cba0b56cc26d89bdc6d56cd3bd7a9fdef910b88c
sinna <simon_solutions@outlook.com>
2025-08-03 11:21:23 +0200

    add drop down atom

--- src/components/molecules/BlocContactNO.tsx
+++ src/components/molecules/BlocContactNO.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import clsx from 'clsx';
 import { getColorByValue } from '@/utils/getColorByValue';
-import { text } from 'stream/consumers';
+import DropDown_IC from '@/components/atoms/DropDown_IC';
 
 type BlocContactNOProps = {
   composantsUnity: string[];
@@ -46,18 +46,15 @@ const BlocContactNO = ({
       />
 
       <div className="flex flex-col items-center gap-3">
-        <select
-          className="w-full rounded-xl border border-gray-300 bg-white px-3 py-2 text-sm shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none"
+        <DropDown_IC
+          composantsUnity={composantsUnity}
           value={selectedSensor}
-          onChange={(e) => setSelectedSensor(e.target.value)}
-        >
-          <option value="">???</option>
-          {composantsUnity.map((name) => (
-            <option key={name} value={name}>
-              {name}
-            </option>
-          ))}
-        </select>
+          onChange={setSelectedSensor}
+          placeholder="???"
+          showSensor
+          showMemoire
+          showActionneur
+        />
 
         <span
           className={clsx(
